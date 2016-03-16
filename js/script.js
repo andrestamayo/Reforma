4f63405c94fbdabeffadbdcb6f0af18091844657
andrestamayo <andybmsek@gmail.com>
2016-03-16 13:33:52 +0100

    Added files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.res').mouseenter(function(){
+        $(this).fadeTo('fast',1)}
+        );
+    $('.res').mouseleave(function(){
+        $(this).fadeTo('fast',0.5)
+        })
+    });
